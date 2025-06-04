dddaa9df4378a7ebec7ebc61e73b1090a4352845
varshabh <varshabh@uw.edu>
2025-06-04 06:23:53 -0700

    Initial commit with storyboard-only layout

---
+++ IRL Bingo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IRL Bingo
+//
+//  Created by Varsha Bharath on 6/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IRL BingoTests/IRL_BingoTests.swift
@@ -0,0 +1,17 @@
+//
+//  IRL_BingoTests.swift
+//  IRL BingoTests
+//
+//  Created by Varsha Bharath on 6/4/25.
+//
+
+import Testing
+@testable import IRL_Bingo
+
+struct IRL_BingoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
